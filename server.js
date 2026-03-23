f73adbec413aa098bdf217242ae2452dd3f90f02
Milana Ustinova <131642299+MyatnoeVino@users.noreply.github.com>
2026-03-23 09:15:11 +0200

    Fix catch-all route syntax in server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get('/config', (req, res) => {
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
+// Используем правильный синтаксис для catch-all
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 

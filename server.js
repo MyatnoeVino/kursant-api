b53fa8c8b616408307692bfcdcd824ab8ded1a3c
Milana Ustinova <131642299+MyatnoeVino@users.noreply.github.com>
2026-03-23 09:21:37 +0200

    Refactor routes and comments in server.js

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Порт из .env или 3000 по умолчанию
 const PORT = parseInt(process.env.PORT, 10) || 3000;
-
-// URL вашего PocketBase в Coolify
 const PB_URL = 'http://pocketbase-fvz5oclyfmgiesbdipcfquzc:8080';
 
-// Раздаём статические файлы (index.html, front.js, css и т.д.)
+// Статика фронтенда
 app.use(express.static(__dirname));
 
 // Роут для передачи конфигурации фронтенду
@@ -17,12 +14,12 @@ app.get('/config', (req, res) => {
     res.json({ pbUrl: PB_URL });
 });
 
-// Wildcard роут: любой путь возвращает index.html
-app.get('/*', (req, res) => {
+// Wildcard для фронтенда
+// В Express 4 нужно просто '*', а не '/*'
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Запуск сервера
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
